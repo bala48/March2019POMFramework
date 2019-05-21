dacbd53a25778e039cd989422465c82afdad7e6c
balaji <bala48@gmail.com>
2019-05-21 18:39:55 +0530

    added deals page class

---
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -0,0 +1,10 @@
+/**
+ * Practicing by b to the a to the l to the a to the j to the i
+ */
+package com.qa.hubspot.pages;
+
+import com.qa.hubspot.base.BasePage;
+
+public class DealsPage extends BasePage {
+
+}
